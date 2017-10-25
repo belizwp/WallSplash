3c6e0f8a40b9e6dc0cd71e02882fdc0566586c8d
belizwp <belizwp@live.com>
2017-10-25 20:30:28 +0700

    fecth photos

--- app/src/main/java/kmitl/afinal/nakarin58070064/wallsplash/adapter/WallpaperListAdapter.java
+++ app/src/main/java/kmitl/afinal/nakarin58070064/wallsplash/adapter/WallpaperListAdapter.java
@@ -1,14 +1,29 @@
 package kmitl.afinal.nakarin58070064.wallsplash.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import kmitl.afinal.nakarin58070064.wallsplash.R;
 import kmitl.afinal.nakarin58070064.wallsplash.adapter.holder.WallpaperHolder;
+import kmitl.afinal.nakarin58070064.wallsplash.model.Photo;
 
 public class WallpaperListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context mContext;
+    private List<Photo> photoList;
+
+    public WallpaperListAdapter(Context context) {
+        mContext = context;
+        photoList = new ArrayList<>();
+    }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -22,12 +37,19 @@ public class WallpaperListAdapter extends RecyclerView.Adapter<RecyclerView.View
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         WallpaperHolder wallpaperHolder = (WallpaperHolder) holder;
-        wallpaperHolder.imageView.setImageResource(R.drawable.happy);
+        Photo photo = photoList.get(position);
+
+        Glide.with(mContext)
+                .load(photo.getUrls().getSmall())
+                .into(wallpaperHolder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 20;
+        return photoList.size();
     }
 
+    public void setPhotoList(List<Photo> photoList) {
+        this.photoList = photoList;
+    }
 }
--- app/src/main/java/kmitl/afinal/nakarin58070064/wallsplash/fragment/WallpaperListFragment.java
+++ app/src/main/java/kmitl/afinal/nakarin58070064/wallsplash/fragment/WallpaperListFragment.java
@@ -2,37 +2,56 @@ package kmitl.afinal.nakarin58070064.wallsplash.fragment;
 
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import kmitl.afinal.nakarin58070064.wallsplash.R;
 import kmitl.afinal.nakarin58070064.wallsplash.adapter.WallpaperListAdapter;
 import kmitl.afinal.nakarin58070064.wallsplash.model.GridSpacingItemDecoration;
+import kmitl.afinal.nakarin58070064.wallsplash.model.Photo;
+import kmitl.afinal.nakarin58070064.wallsplash.network.ApiManager;
+import kmitl.afinal.nakarin58070064.wallsplash.network.UnsplashAPI;
 import kmitl.afinal.nakarin58070064.wallsplash.util.ScreenUtils;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class WallpaperListFragment extends Fragment {
+
+    private static final String KEY_PHOTO_LIST = "PHOTO_LIST";
 
     private RecyclerView rvWallpaperList;
     private TextView tvError;
     private WallpaperListAdapter adapter;
 
+    private UnsplashAPI api;
+    private List<Photo> photoList;
+
     public static WallpaperListFragment newInstance() {
         WallpaperListFragment fragment = new WallpaperListFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-        }
+        api = ApiManager.getInstance().getUnsplashApi(getContext().getString(R.string.application_id));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(KEY_PHOTO_LIST, (ArrayList<? extends Parcelable>) photoList);
     }
 
     @Override
@@ -47,12 +66,41 @@ public class WallpaperListFragment extends Fragment {
         rvWallpaperList = rootView.findViewById(R.id.rvWallpaperList);
         tvError = rootView.findViewById(R.id.tvError);
 
-        adapter = new WallpaperListAdapter();
+        adapter = new WallpaperListAdapter(getContext());
         rvWallpaperList.setLayoutManager(new GridLayoutManager(getContext(), 3));
         int space = (int) ScreenUtils.convertDpToPixel(8, getContext());
         rvWallpaperList.addItemDecoration(new GridSpacingItemDecoration(3, space, true));
         rvWallpaperList.setAdapter(adapter);
 
+        if (savedInstanceState != null) {
+            photoList = savedInstanceState.getParcelableArrayList(KEY_PHOTO_LIST);
+            display();
+        } else {
+            getPhotos();
+        }
     }
 
+    private void display() {
+        adapter.setPhotoList(photoList);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void getPhotos() {
+        Call<List<Photo>> call = api.getPhotos(null, null, null);
+        call.enqueue(new Callback<List<Photo>>() {
+            @Override
+            public void onResponse(Call<List<Photo>> call, Response<List<Photo>> response) {
+                Log.d("API", "onResponse");
+                if (response.isSuccessful()) {
+                    photoList = response.body();
+                    display();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Photo>> call, Throwable t) {
+                Log.d("API", "onFailure " + t.getMessage());
+            }
+        });
+    }
 }
