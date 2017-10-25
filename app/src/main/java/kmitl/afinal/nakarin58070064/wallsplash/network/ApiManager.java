6d135c39a616277c58ad9d28708417637f37a5cb
belizwp <belizwp@live.com>
2017-10-25 20:27:37 +0700

    enable tls1.2 for api below 22

--- app/src/main/java/kmitl/afinal/nakarin58070064/wallsplash/network/ApiManager.java
+++ app/src/main/java/kmitl/afinal/nakarin58070064/wallsplash/network/ApiManager.java
@@ -1,11 +1,26 @@
 package kmitl.afinal.nakarin58070064.wallsplash.network;
 
-import java.io.IOException;
+import android.os.Build;
+import android.util.Log;
 
+import java.io.IOException;
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.ConnectionSpec;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.TlsVersion;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -28,8 +43,8 @@ public class ApiManager {
 
     public UnsplashAPI getUnsplashApi(final String applicationID) {
         if (unsplashAPI == null) {
-            OkHttpClient client = new OkHttpClient
-                    .Builder()
+
+            OkHttpClient client = getNewHttpClient()
                     .addInterceptor(new Interceptor() {
                         @Override
                         public Response intercept(Chain chain) throws IOException {
@@ -39,8 +54,7 @@ public class ApiManager {
                                     .build();
                             return chain.proceed(request);
                         }
-                    })
-                    .build();
+                    }).build();
 
             Retrofit retrofit = new Retrofit
                     .Builder()
@@ -52,5 +66,53 @@ public class ApiManager {
             unsplashAPI = retrofit.create(UnsplashAPI.class);
         }
         return unsplashAPI;
+    }
+
+    public static OkHttpClient.Builder enableTls12OnPreLollipop(OkHttpClient.Builder client) {
+        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
+            try {
+                TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                        TrustManagerFactory.getDefaultAlgorithm());
+                trustManagerFactory.init((KeyStore) null);
+                TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+                if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                    throw new IllegalStateException("Unexpected default trust managers:"
+                            + Arrays.toString(trustManagers));
+                }
+                X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
+
+                SSLContext sc = SSLContext.getInstance("TLSv1.2");
+                sc.init(null, new TrustManager[]{trustManager}, null);
+                client.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()), trustManager);
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                client.connectionSpecs(specs);
+            } catch (Exception exc) {
+                Log.e("OkHttpTLSCompat", "Error while setting TLS 1.2", exc);
+            }
+        }
+
+        return client;
+    }
+
+    private OkHttpClient.Builder getNewHttpClient() {
+        OkHttpClient.Builder client = new OkHttpClient.Builder()
+                .followRedirects(true)
+                .followSslRedirects(true)
+                .retryOnConnectionFailure(true)
+                .cache(null)
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .writeTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS);
+
+        return enableTls12OnPreLollipop(client);
     }
 }
