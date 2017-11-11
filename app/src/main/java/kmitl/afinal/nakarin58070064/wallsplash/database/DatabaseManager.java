8a96532f09f15bd1e91e78682f32dbf4cbea3afb
belizwp <belizwp@live.com>
2017-11-12 00:04:13 +0700

    create default collection at first run

--- app/src/main/java/kmitl/afinal/nakarin58070064/wallsplash/database/DatabaseManager.java
+++ app/src/main/java/kmitl/afinal/nakarin58070064/wallsplash/database/DatabaseManager.java
@@ -2,8 +2,13 @@ package kmitl.afinal.nakarin58070064.wallsplash.database;
 
 import android.arch.persistence.room.Room;
 import android.content.Context;
+import android.content.SharedPreferences;
 
+import kmitl.afinal.nakarin58070064.wallsplash.BuildConfig;
 import kmitl.afinal.nakarin58070064.wallsplash.R;
+import kmitl.afinal.nakarin58070064.wallsplash.model.Collection;
+import kmitl.afinal.nakarin58070064.wallsplash.model.MyCollection;
+import kmitl.afinal.nakarin58070064.wallsplash.task.AddCollectionTask;
 
 public class DatabaseManager {
 
@@ -16,6 +21,7 @@ public class DatabaseManager {
         return instance;
     }
 
+    private final String KEY_FIRST_RUN = "FIRST_RUN";
     private WallSplashDatabase mDatabase;
 
     private DatabaseManager() {
@@ -27,6 +33,21 @@ public class DatabaseManager {
                 context.getResources().getString(R.string.db_name))
                 .fallbackToDestructiveMigration()
                 .build();
+
+        final SharedPreferences prefs = context.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE);
+
+        if (prefs.getBoolean(KEY_FIRST_RUN, true)) {
+            // create default collection for the first time using app
+            MyCollection myCollection = new MyCollection();
+            myCollection.setTitle("My Favorite");
+
+            new AddCollectionTask(mDatabase, new AddCollectionTask.OnPostAddListener() {
+                @Override
+                public void onPostAdd(long id) {
+                    prefs.edit().putBoolean(KEY_FIRST_RUN, false).apply();
+                }
+            }).execute(myCollection);
+        }
     }
 
     public WallSplashDatabase getDatabase() {
